50e3ed9eaec063afbb1d9b9852b9fe11c5f67e51
Drew Mooney <mamooney12@gmail.com>
2015-12-21 20:54:38 -0600

    First commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,15 @@
+class PagesController < ApplicationController
+
+	def home
+	end
+	
+	def about
+	end
+	
+	def post
+	end
+	
+	def contact
+	end
+
+end
---
+++ config/initializers/assets.rb
@@ -0,0 +1,32 @@
+# Be sure to restart your server when you modify this file.
+
+# Version of your assets, change this if you want to expire all your assets.
+Rails.application.config.assets.version = '1.0'
+
+# CSS files
+Rails.application.config.assets.precompile += %w( bootstrap.min.css )
+Rails.application.config.assets.precompile += %w( bootstrap.css )
+Rails.application.config.assets.precompile += %w( clean-blog.css )
+Rails.application.config.assets.precompile += %w( clean-blog.min.css )
+
+# JS files
+Rails.application.config.assets.precompile += %w( bootstrap.js )
+Rails.application.config.assets.precompile += %w( bootstrap.min.js )
+Rails.application.config.assets.precompile += %w( clean-blog.js )
+Rails.application.config.assets.precompile += %w( clean-blog.min.js )
+Rails.application.config.assets.precompile += %w( jquery.js )
+Rails.application.config.assets.precompile += %w( jquery.min.js )
+
+# images
+Rails.application.config.assets.precompile += %w( about-bg.jpg )
+Rails.application.config.assets.precompile += %w( contact-bg.jpg )
+Rails.application.config.assets.precompile += %w( home-bg.jpg )
+Rails.application.config.assets.precompile += %w( post-bg.jpg )
+Rails.application.config.assets.precompile += %w( post-sample-image-bg.jpg )
+
+# Add additional assets to the asset load path
+# Rails.application.config.assets.paths << Emoji.images_path
+
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
+# Rails.application.config.assets.precompile += %w( search.js )
